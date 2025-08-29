4b490c467f725986ebdcbca993fda458107f453f
A. Finkhaeuser <alex@elaon.de>
2025-08-30 00:27:42 +0200

    simpler nur uart empfangen udn ausgeben.

--- components/jhs_climate/jhs_climate.cpp
+++ components/jhs_climate/jhs_climate.cpp
@@ -1,6 +1,6 @@
 #include "jhs_climate.h"
 
-static const char *TAG = "JHSClimateComp";
+static const char *TAG = "JHSClimate";
 
 namespace JHS {
 
@@ -12,20 +12,9 @@ void JHSClimate::update() {
   uint8_t b;
   while (this->available()) {
     if (this->read_byte(&b)) {
-      buffer_.push_back(b);
-      const size_t packet_len = sizeof(JHSAcPacket) + 1;
-      if (buffer_.size() >= packet_len) {
-        std::vector<uint8_t> candidate(buffer_.begin(), buffer_.begin() + packet_len);
-        auto pkt = JHSAcPacket::parse(candidate);
-        if (pkt.has_value()) {
-          ESP_LOGI(TAG, "Paket erkannt: %s", pkt->to_string().c_str());
-          buffer_.erase(buffer_.begin(), buffer_.begin() + packet_len);
-        } else {
-          buffer_.erase(buffer_.begin());
-        }
-      }
+      ESP_LOGI(TAG, "UART received: 0x%02X ('%c')", b, (b >= 32 && b <= 126) ? b : '.');
     }
   }
 }
 
-} // namespace JHS
+}  // namespace JHS
--- components/jhs_climate/jhs_climate.h
+++ components/jhs_climate/jhs_climate.h
@@ -1,18 +1,14 @@
 #pragma once
 #include "esphome.h"
-#include "jhs_packets.h"
 
 namespace JHS {
 
 class JHSClimate : public PollingComponent, public uart::UARTDevice {
-    public:
-    explicit JHSClimate(esphome::uart::UARTComponent *parent) : UARTDevice(parent) {}
+public:
+  explicit JHSClimate(esphome::uart::UARTComponent *parent) : UARTDevice(parent) {}
 
-    void setup() override;
-    void update() override;
-
-    private:
-    std::vector<uint8_t> buffer_;
+  void setup() override;
+  void update() override;
 };
 
 }  // namespace JHS
