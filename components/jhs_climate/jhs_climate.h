268129605d2c09f3925a5302bd11a110a18d8421
A. Finkhaeuser <alex@elaon.de>
2025-08-29 23:53:00 +0200

    neuer test mit eigenem code. erst mal decodieren udn ausgeben

--- components/jhs_climate/jhs_climate.h
+++ components/jhs_climate/jhs_climate.h
@@ -1,81 +1,26 @@
 #pragma once
-//#include <esp32-hal-log.h>
-#include "esphome/core/component.h"
-#include "esphome/core/hal.h"
-#include "esphome/core/gpio.h"
-#include "esphome/components/climate/climate.h"
-#include "esphome/components/binary_sensor/binary_sensor.h"
-#include "esphome/components/wifi/wifi_component.h"
-//#include "esphome/components/ota/ota_component.h"
+
+
 #include "esphome.h"
-
-//#include "esp32-hal-rmt.h"
-#include "soc/rmt_struct.h"
 #include "jhs_packets.h"
-#include <vector>
 
 
-namespace esphome
-{
-namespace JHSClimate
-{
+namespace JHS {
 
-class JHSClimate : public esphome::Component, public esphome::climate::Climate
-{
+
+class JHSClimate : public esphome::Component, public esphome::uart::UARTDevice {
 public:
-    // pin setters
-    void set_ac_tx_pin(esphome::InternalGPIOPin *ac_tx_pin) { ac_tx_pin_ = ac_tx_pin; }
-    void set_ac_rx_pin(esphome::InternalGPIOPin *ac_rx_pin) { ac_rx_pin_ = ac_rx_pin; }
-    void set_panel_tx_pin(esphome::InternalGPIOPin *panel_tx_pin) { panel_tx_pin_ = panel_tx_pin; }
-    void set_panel_rx_pin(esphome::InternalGPIOPin *panel_rx_pin) { panel_rx_pin_ = panel_rx_pin; }
+explicit JHSClimate(esphome::uart::UARTComponent *parent) : UARTDevice(parent) {}
 
-    void set_water_full_sensor(esphome::binary_sensor::BinarySensor *water_full_sensor_) { water_full_sensor  = water_full_sensor_; }
 
-    // esphome handlers
-    void setup() override;
-    void dump_config() override;
-    esphome::climate::ClimateTraits traits() override;
-    void control(const esphome::climate::ClimateCall &call) override;
-    void loop() override;
+void setup() override;
+void loop() override;
+void dump_config() override;
 
-protected:
-    esphome::InternalGPIOPin *ac_tx_pin_;
-    esphome::InternalGPIOPin *ac_rx_pin_;
-    esphome::InternalGPIOPin *panel_tx_pin_;
-    esphome::InternalGPIOPin *panel_rx_pin_;
-    esphome::binary_sensor::BinarySensor *water_full_sensor;
-    // esphome::ota::OTAComponent *OTAComponent =
-
-    rmt_obj_t *rmt_ac_tx;
-    rmt_obj_t *rmt_panel_tx;
-
-    uint32_t last_adjustment = 0;
-    const int ADJUSTMENT_INTERVAL = 100;
-    int steps_left_to_adjust_mode = 0;
-    int steps_left_to_adjust_temp = 0;
-    int steps_left_to_adjust_fan = 0;
-    bool adjust_preset = false;
-    bool water_full = false;
-    uint32_t last_water_full = 0;
-    const int WATER_FULL_INTERVAL = 3000;
-
-    // is_adjusting is set to true when a change was made externally (e.g. homeassistant) and we are in the process of pressing button
-    bool is_adjusting();
-
-    float rmt_panel_tx_tick;
-    float rmt_ac_tx_tick;
 
 private:
-    // setup helpers
-    void setup_rmt();
-
-    void send_rmt_data(rmt_obj_t *rmt, std::vector<uint8_t> data);
-
-    void recv_from_panel();
-
-    void recv_from_ac();
-
-    void update_screen_if_needed();
+std::vector<uint8_t> buffer_;
 };
-}
-}
+
+
+} // namespace JHS
--- components/jhs_climate/jhs_packets.h
+++ components/jhs_climate/jhs_packets.h
@@ -1,15 +1,15 @@
 #pragma once
 
+
 #include <cstdint>
 #include <string>
-
-#include <map>
 #include <vector>
 #include <array>
-#include <map>
 #include "esphome/core/optional.h"
 
+
 using namespace esphome;
+
 
 const std::array<uint8_t, 3> KEEPALIVE_PACKET{0x30, 0x00, 0x8a};
 const std::array<uint8_t, 3> BUTTON_MODE{0x30, 0x01, 0x8b};
@@ -17,7 +17,6 @@ const std::array<uint8_t, 3> BUTTON_LOWER_TEMP{0x30, 0x02, 0x8c};
 const std::array<uint8_t, 3> BUTTON_ON{0x30, 0x03, 0x8d};
 const std::array<uint8_t, 3> BUTTON_TIMER{0x30, 0x04, 0x8e};
 const std::array<uint8_t, 3> BUTTON_FAN{0x30, 0x05, 0x8f};
-// const std::array<uint8_t, 3> BUTTON_SWING{0x30, 0x06, 0x90};
 const std::array<uint8_t, 3> BUTTON_SLEEP{0x30, 0x07, 0x91};
 const std::array<uint8_t, 3> BUTTON_HIGHER_TEMP{0x30, 0x08, 0x92};
 const std::array<uint8_t, 3> BUTTON_UNIT_CHANGE{0x30, 0x09, 0x93};
@@ -26,45 +25,56 @@ const std::array<uint8_t, 3> BUTTON_UNIT_CHANGE{0x30, 0x09, 0x93};
 ///@brief Packet sent from the AC to the panel.
 struct JHSAcPacket
 {
-    uint8_t addr = 0x90; // always 0x90
+uint8_t addr = 0x90; // always 0x90
 
-    uint8_t first_digit = 0; // bits 0-7
 
-    uint8_t second_digit = 0; // bits 8-15
+uint8_t first_digit = 0; // bits 0-7
 
-    uint8_t zero0 = 0; // bits 16-23
 
-    uint8_t zero1 = 0; // bits 24-31
+uint8_t second_digit = 0; // bits 8-15
 
-    uint8_t cool : 1 = 0;       // bit 32
-    uint8_t dehum : 1 = 0;      // bit 33
-    uint8_t fan : 1 = 0;        // bit 34
-    uint8_t heat : 1 = 0;       // bit 35
-    uint8_t sleep : 1 = 0;      // bit 36
-    uint8_t water_full : 1 = 0; // bit 37
-    uint8_t swing : 1 = 0;      // bit 38
-    uint8_t timer : 1 = 0;      // bit 39
 
-    uint8_t fan_low : 1 = 0;    // bit 40
-    uint8_t fan_unused : 1 = 0; // bit 41
-    uint8_t fan_high : 1 = 0;   // bit 42
-    uint8_t wifi : 1 = 0;    // bit 43
-    uint8_t unused_above_timer : 1 = 0;    // bit 44
-    uint8_t power : 1 = 1;      // bit 45
-    uint8_t unused4 : 1 = 0;    // bit 46
-    uint8_t unused5 : 1 = 0;    // bit 47
+uint8_t zero0 = 0; // bits 16-23
 
-    uint8_t beep_length : 4 = 0;
-    uint8_t beep_amount : 4 = 0;
 
-    std::string to_string();
+uint8_t zero1 = 0; // bits 24-31
 
-    void set_temp(int);
-    int get_temp();
-    void set_display(std::string);
 
-    ///@brief Parses the packet and checks the checksum.
-    static esphome::optional<JHSAcPacket> parse(const std::vector<uint8_t> &data);
+uint8_t cool : 1 = 0; // bit 32
+uint8_t dehum : 1 = 0; // bit 33
+uint8_t fan : 1 = 0; // bit 34
+uint8_t heat : 1 = 0; // bit 35
+uint8_t sleep : 1 = 0; // bit 36
+uint8_t water_full : 1 = 0; // bit 37
+uint8_t swing : 1 = 0; // bit 38
+uint8_t timer : 1 = 0; // bit 39
 
-    std::vector<uint8_t> to_wire_format();
+
+uint8_t fan_low : 1 = 0; // bit 40
+uint8_t fan_unused : 1 = 0; // bit 41
+uint8_t fan_high : 1 = 0; // bit 42
+uint8_t wifi : 1 = 0; // bit 43
+uint8_t unused_above_timer : 1 = 0; // bit 44
+uint8_t power : 1 = 1; // bit 45
+uint8_t unused4 : 1 = 0; // bit 46
+uint8_t unused5 : 1 = 0; // bit 47
+
+
+uint8_t beep_length : 4 = 0;
+uint8_t beep_amount : 4 = 0;
+
+
+std::string to_string();
+
+
+void set_temp(int);
+int get_temp();
+void set_display(std::string);
+
+
+///@brief Parses the packet and checks the checksum.
+static esphome::optional<JHSAcPacket> parse(const std::vector<uint8_t> &data);
+
+
+std::vector<uint8_t> to_wire_format();
 } __attribute__((packed));
