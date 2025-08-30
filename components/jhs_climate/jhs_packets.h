d40eb15a559c4dc757921ef872d046beff5b9b21
A. Finkhaeuser <alex@elaon.de>
2025-08-30 02:01:19 +0200

    korrektur der ehader datei

--- components/jhs_climate/jhs_packets.h
+++ components/jhs_climate/jhs_packets.h
@@ -6,9 +6,8 @@
 
 namespace JHS {
 
-// Paketstruktur vom AC
 struct JHSAcPacket {
-  uint8_t addr = 0x90;  // immer 0x90
+  uint8_t addr = 0x90;
   uint8_t first_digit = 0;
   uint8_t second_digit = 0;
   uint8_t zero0 = 0;
@@ -32,23 +31,9 @@ struct JHSAcPacket {
   uint8_t beep_length : 4 = 0;
   uint8_t beep_amount : 4 = 0;
 
-  std::string to_string() {
-    std::stringstream ss;
-    ss << "DISP:" << (int)first_digit << (int)second_digit;
-    if (power) ss << " POWER";
-    if (cool) ss << " COOL";
-    if (heat) ss << " HEAT";
-    if (fan) ss << " FAN";
-    if (dehum) ss << " DEHUM";
-    return ss.str();
-  }
-
-  static JHSAcPacket parse(const std::vector<uint8_t> &data) {
-    JHSAcPacket p;
-    if (data.size() < sizeof(JHSAcPacket) + 1) return p;
-    memcpy(&p, data.data(), sizeof(JHSAcPacket));
-    return p;
-  }
+  // Methoden nur deklarieren
+  std::string to_string() const;
+  static JHSAcPacket parse(const std::vector<uint8_t> &data);
 } __attribute__((packed));
 
 }  // namespace JHS
