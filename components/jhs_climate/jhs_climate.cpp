ef71f998f7901b8861eefaf7c37a0500248c73ba
A. Finkhaeuser <alex@elaon.de>
2025-08-31 23:05:47 +0200

    korrektur function deklaration

--- components/jhs_climate/jhs_climate.cpp
+++ components/jhs_climate/jhs_climate.cpp
@@ -365,7 +365,7 @@ void JHSClimate::recv_from_ac()
     }
 }
 
-void send_rmt_data(rmt_channel_t rmt_channel, const std::vector<uint8_t> &data) {
+void JHSClimate::send_rmt_data(rmt_channel_t rmt_channel, const std::vector<uint8_t> &data) {
     if (!rmt_channel) return;
 
     ESP_LOGVV(TAG, "Sending RMT data: %s", bytes_to_hex2(data).c_str());
