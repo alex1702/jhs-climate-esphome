cd01c6ffa52b3441a3f2efa92d026e321e11c1f1
A. Finkhaeuser <alex@elaon.de>
2025-08-31 16:29:17 +0200

    imports korrigiert und Datentypen angepasst

--- components/jhs_climate/jhs_recv_task.h
+++ components/jhs_climate/jhs_recv_task.h
@@ -8,9 +8,11 @@
 #include <freertos/task.h>
 #include <freertos/queue.h>
 
+#include "esphome/core/gpio.h"
+
 namespace JHS {
 
-const size_t JHS_AC_PACKET_SIZE = (sizeof(JHSAcPacket) + 1);
+const size_t JHS_AC_PACKET_SIZE = (sizeof(JHS::JHSAcPacket) + 1);
 const size_t JHS_PANEL_PACKET_SIZE = 3;
 
 extern volatile QueueHandle_t ac_rx_queue;
@@ -18,8 +20,8 @@ extern volatile QueueHandle_t panel_rx_queue;
 
 struct jhs_recv_task_config
 {
-    gpio_num_t ac_rx_pin;
-    gpio_num_t panel_rx_pin;
+    esphome::InternalGPIOPin ac_rx_pin;
+    esphome::InternalGPIOPin panel_rx_pin;
 };
 
 void start_jhs_climate_recv_task(jhs_recv_task_config config);
