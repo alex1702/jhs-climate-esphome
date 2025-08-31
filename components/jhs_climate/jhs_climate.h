5aa61c0f34de68ac7967bb792e81e0567e0f1424
A. Finkhaeuser <alex@elaon.de>
2025-09-01 00:28:13 +0200

    wifi import

--- components/jhs_climate/jhs_climate.h
+++ components/jhs_climate/jhs_climate.h
@@ -4,7 +4,7 @@
 #include "esphome/core/gpio.h"
 #include "esphome/components/climate/climate.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
-//#include "esphome/components/wifi/wifi_component.h"
+#include "esphome/components/wifi/wifi_component.h"
 
 #include "driver/rmt.h"
 
