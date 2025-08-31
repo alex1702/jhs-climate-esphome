8c0d42c07e295b50888b84e3b5c9ac7a4fd53964
A. Finkhaeuser <alex@elaon.de>
2025-09-01 00:51:05 +0200

    fehlende funktionen hinzugefügt

--- components/jhs_climate/jhs_climate.cpp
+++ components/jhs_climate/jhs_climate.cpp
@@ -101,6 +101,50 @@ void JHSClimate::loop()
     this->recv_from_panel();
 }
 
+void JHSClimate::control(const esphome::climate::ClimateCall &call)
+{
+    if (call.get_target_temperature().has_value())
+    {
+        this->target_temperature = call.get_target_temperature().value();
+        this->steps_left_to_adjust_temp = 24;
+    }
+    if (call.get_mode().has_value())
+    {
+        this->mode = call.get_mode().value();
+        this->steps_left_to_adjust_mode = 8;
+    }
+    if (call.get_fan_mode().has_value())
+    {
+        this->fan_mode = call.get_fan_mode().value();
+        this->steps_left_to_adjust_fan = 1;
+    }
+    if (call.get_preset().has_value())
+    {
+        this->preset = call.get_preset().value();
+        this->adjust_preset = true;
+    }
+    this->publish_state();
+}
+
+esphome::climate::ClimateTraits JHSClimate::traits()
+{
+    // The capabilities of the climate device
+    auto traits = esphome::climate::ClimateTraits();
+    traits.set_supports_current_temperature(true);
+    traits.set_supported_modes({esphome::climate::CLIMATE_MODE_OFF,
+                                esphome::climate::CLIMATE_MODE_DRY,
+                                esphome::climate::CLIMATE_MODE_COOL,
+                                esphome::climate::CLIMATE_MODE_FAN_ONLY});
+    traits.set_supported_fan_modes({esphome::climate::CLIMATE_FAN_LOW,
+                                    esphome::climate::CLIMATE_FAN_HIGH});
+    traits.set_supported_presets({esphome::climate::CLIMATE_PRESET_NONE,
+                                  esphome::climate::CLIMATE_PRESET_SLEEP});
+    traits.set_visual_min_temperature(16);
+    traits.set_visual_max_temperature(31);
+    traits.set_visual_temperature_step(1);
+    return traits;
+}
+
 void JHSClimate::recv_from_panel()
 {
     uint8_t packet[JHS_PANEL_PACKET_SIZE];
