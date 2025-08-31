2c8b0cf97454685b35db77f889e4fa267fd9e00b
A. Finkhaeuser <alex@elaon.de>
2025-08-31 17:01:13 +0200

    fehlender namespace

--- components/jhs_climate/jhs_recv_task.h
+++ components/jhs_climate/jhs_recv_task.h
@@ -12,7 +12,7 @@
 
 namespace JHS {
 
-const size_t JHS_AC_PACKET_SIZE = (sizeof(JHSAcPacket) + 1);
+const size_t JHS_AC_PACKET_SIZE = (sizeof(JHS::JHSAcPacket) + 1);
 const size_t JHS_PANEL_PACKET_SIZE = 3;
 
 extern volatile QueueHandle_t ac_rx_queue;
