6661c96bdf83e016cd4cfe9f54735ffdae2c1bd3
A. Finkhaeuser <alex@elaon.de>
2025-09-03 08:44:10 +0200

    Aufräumarbeit

--- components/jhs_climate/__init__.py
+++ components/jhs_climate/__init__.py
@@ -20,16 +20,6 @@ CONF_PANEL_TX_PIN = 'panel_tx_pin'
 CONF_PANEL_RX_PIN = 'panel_rx_pin'
 CONF_WATER_FULL_SENSOR = 'water_full_sensor'
 
-# CONFIG_SCHEMA = cv.Schema(
-#     {
-#         cv.GenerateID(): cv.declare_id(JHSClimate),
-#         cv.Required(CONF_AC_TX_PIN): pins.gpio_output_pin_schema,
-#         cv.Required(CONF_AC_RX_PIN): pins.gpio_input_pin_schema,
-#         cv.Required(CONF_PANEL_TX_PIN): pins.gpio_output_pin_schema,
-#         cv.Required(CONF_PANEL_RX_PIN): pins.gpio_input_pin_schema,
-#     }
-# )
-
 CONFIG_SCHEMA = cv.All(
     climate.climate_schema(JHSClimate).extend(
         {
@@ -43,19 +33,7 @@ CONFIG_SCHEMA = cv.All(
     )
 )
 
-# später diese schema beschreibung
-# CONFIG_SCHEMA = climate.climate_schema(
-#     {
-#         cv.GenerateID(): cv.declare_id(JHSClimate),
-#         cv.Required(CONF_AC_TX_PIN): pins.gpio_output_pin_schema,
-#         cv.Required(CONF_AC_RX_PIN): pins.gpio_input_pin_schema,
-#         cv.Required(CONF_PANEL_TX_PIN): pins.gpio_output_pin_schema,
-#         cv.Required(CONF_PANEL_RX_PIN): pins.gpio_input_pin_schema,
-#         cv.Required(CONF_WATER_FULL_SENSOR): binary_sensor.binary_sensor_schema(),
-#     }
-# )
 # CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
-# # CONFIG_SCHEMA = CONFIG_SCHEMA.extend(
 #     {
 #         cv.GenerateID(): cv.declare_id(JHSClimate),
 #         cv.Required(CONF_AC_TX_PIN): pins.gpio_output_pin_schema,
@@ -63,7 +41,6 @@ CONFIG_SCHEMA = cv.All(
 #         cv.Required(CONF_PANEL_TX_PIN): pins.gpio_output_pin_schema,
 #         cv.Required(CONF_PANEL_RX_PIN): pins.gpio_input_pin_schema,
 #         cv.Required(CONF_WATER_FULL_SENSOR): binary_sensor.binary_sensor_schema(),
-        
 #     }
 # )
 
